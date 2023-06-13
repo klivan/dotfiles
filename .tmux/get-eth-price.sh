e2ea6bda37b0b2775913fc4660352de12add776f
Ivan Klishch <ivan@klishch.com>
2023-06-13 15:44:48 -0400

    New changes

--- .tmux/get-eth-price.sh
+++ .tmux/get-eth-price.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s https://api.coinbase.com/v2/prices/ETH-USD/spot | jq -r '.data.amount'
+curl -s https://api.coinbase.com/v2/prices/ETH-USD/spot | jq -r '.data.amount' | jq '.*100|round/100'
